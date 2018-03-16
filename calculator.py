0f59466a8c010a9e52b6a6705b76b5be64bda87c
364074293 <364074293@qq.com>
2018-03-16 16:25:44 +0800

    wanshan cal

--- calculator.py
+++ calculator.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python3
 import sys
+
+
 try:
-	i=int(sys.argv[1])*(1-0.165)-3500
-	if i>80000:
-		i=i*0.45-13500
-		print(format(i,'.2f'))
-	elif 55000<i<=80000:
-		i=i*0.35-5505
-		print(format(i,'.2f'))
-	elif 35000<i<=55000:
-		i=i*0.3-2755
-		print(format(i,'.2f'))
-	elif 9000<i<=35000:
-		i=i*0.25-1005
-		print(format(i,'.2f'))
-	elif 4500<i<=9000:
-		i=i*0.20-555
-		print(format(i,'.2f'))
-	elif 1500<i<=4500:
-		i=i*0.1-105
-		print(format(i,'.2f'))
-	elif 0<i<=1500:
-		i=i*0.03
-		print(format(i,'.2f'))
-	elif -3500<i<0:
-		i=0
-		print(format(i,'.2f'))
-	else:
-		print('输入有误')
-except ValueError:
-	print('请输入数字：')
+
+    def get_salary(ratio,deductions):
+       
+        salary_after=tax_before - (taxable*ratio-deductions)
+        print(ID+':'+format(salary_after,'.2f'))
+    
+    
+    def get_salary_after():
+        if taxable>80000:
+            get_salary(0.45,13505)
+        elif 55000<taxable<=80000:
+            get_salary(0.35,5505)
+        elif 35000<taxable<=55000:
+            get_salary(0.3,2755)
+        elif 9000<taxable<=35000:
+            get_salary(0.25,1005)
+        elif 4500<taxable<=9000:
+            get_salary(0.2,555)
+        elif 1500<taxable<=4500:
+            get_salary(0.1,105)
+        elif 0<taxable<=1500:
+            get_salary(0.03,0)
+        elif -3500<taxable<0:
+            get_salary(0,0)
+        else:
+            print('输入有误')
+    sys.argv.pop(0)
+    for i in sys.argv:
+        ID=i.split(':')[0]
+        salary_before=i.split(':')[1]
+
+        try:
+            tax_before=int(salary_before)*(1-0.165)
+            taxable=tax_before*(1-0.165)-3500
+            get_salary_after()
+        except:  
+            print("Parameter Error")
+	
+
+        
+except (IndexError,ValueError):
+    print("请输入:'id:salary_before'")
+
